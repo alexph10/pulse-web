76051555bb080ef18e3251b9b82e25f2165df703
ok no <pnhcan1@gmail.com>
2025-11-06 15:54:26 +0700

    robert robertson

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@
 export default function Home() {
   return (
     <div className="relative flex min-h-screen items-center justify-center font-sans bg-black">
-      <main className="relative z-10 flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
-      </main>
     </div>
   );
 }
